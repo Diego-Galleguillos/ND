d70900b903f738e266df9dae71f4496d6821a4a7
Diego-Galleguillos <dpgalleguillos@uc.cl>
2025-01-15 18:24:28 -0800

    server

--- publisher.py
+++ publisher.py
@@ -1,22 +1,54 @@
 import socket
+import threading
+
+clients = []
+
+# Function to handle client connections
+def handle_client(conn, addr):
+    print(f"Connection from {addr} has been established.")
+    
+    # Keep the connection open
+    while True:
+        try:
+            # Receive data from the client
+            data = conn.recv(1024).decode()
+            if not data:
+                break
+            print(f"Received data from {addr}: {data}")
+        except:
+            break
+    
+    # Close the connection when done
+    conn.close()
+    clients.remove(conn)
+    print(f"Connection from {addr} closed.")
+
+# Function to send messages to all connected clients
+def send_messages():
+    while True:
+        message = input("Enter message to send to all clients: ")
+        for conn in clients:
+            try:
+                conn.send(message.encode())
+            except:
+                pass
 
 # Set up the server
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind(('0.0.0.0', 12345))  # Bind to all interfaces on port 12345
-server_socket.listen(1)
+server_socket.listen(6)  # Allow up to 6 connections
 
 print("Server is listening for connections...")
 
-# Accept a connection
-conn, addr = server_socket.accept()
-print(f"Connection from {addr} has been established.")
+# Start a thread to handle sending messages from the console
+sending_thread = threading.Thread(target=send_messages)
+sending_thread.start()
 
-# Receive data from the client
-data = conn.recv(1024).decode()
-print(f"Received data: {data}")
+while True:
+    # Accept a new connection
+    conn, addr = server_socket.accept()
+    clients.append(conn)
 
-# Send a response back to the client
-conn.send("Hello from the server!".encode())
-
-# Close the connection
-conn.close()
+    # Create a new thread to handle the client
+    client_thread = threading.Thread(target=handle_client, args=(conn, addr))
+    client_thread.start()
---
+++ subs.py
@@ -0,0 +1,20 @@
+import socket
+
+# Set up the client
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect(('192.168.86.211', 12345))  # Connect to the server at 192.168.86.211 on port 12345
+
+while True:
+    # Send data to the server
+    message = input("Enter message to send to the server: ")
+    client_socket.send(message.encode())
+
+    # Receive a response from the server
+    data = client_socket.recv(1024).decode()
+    print(f"Received data: {data}")
+
+    if not data:
+        break
+
+# Close the connection
+client_socket.close()
